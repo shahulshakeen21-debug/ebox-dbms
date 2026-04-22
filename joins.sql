38d47a15c4a12ce70ef84dd479ab53e51e625207
shahulshakeen21-debug <shahulshakeen21@gmail.com>
2026-04-22 11:56:43 +0530

    Update and rename joins.sql to Sql joins i-design

--- joins.sql
+++
@@ -1,5 +0,0 @@
-1.Write a query to display all the bills with meter details. Display the records in ascending order based on payable amount.
-SELECT *
-FROM bill b
-JOIN meter m ON b.meter_id = m.id
-ORDER BY b.payable_amount ASC;
